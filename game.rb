fbe7efbbae7c2b6358e3e2f1c57e12100b94a2a7
Lisa Meyer <lisamermey@gmail.com>
2022-05-02 17:49:20 -0400

    Initial commit

---
+++ game.rb
@@ -0,0 +1,27 @@
+class Game
+
+  def initialize
+    @current_player = [@player1, @player2]
+  end
+
+  def show_score
+
+  end
+
+  def check_score
+
+  end
+
+  def declare_winner
+
+  end
+
+  def turn_order
+
+  end
+
+  def start
+
+  end
+
+end
