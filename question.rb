e2a328b5b165fa53530a29683714894c285b3b61
Lisa Meyer <lisamermey@gmail.com>
2022-05-02 17:56:35 -0400

    Add methods

--- question.rb
+++ question.rb
@@ -1,4 +1,18 @@
 class Question
+  attr_accessor :player_answer
 
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @sum = @num1 + @num2
+  end
+
+  def ask_question
+
+  end
+
+  def check_answer(player_answer)
+    @sum == player_answer
+  end
   
 end
