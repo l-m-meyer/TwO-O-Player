f9d5128fcbda013022a4bce31faee4e3fbab6b12
Lisa Meyer <lisamermey@gmail.com>
2022-05-02 19:16:02 -0400

    Add logic to methods

--- game.rb
+++ game.rb
@@ -1,27 +1,42 @@
 class Game
 
   def initialize
-    @current_player = [@player1, @player2]
+    @player1 = Player.new("Player 1")
+    @player2 = Player.new("Player 2")
+  end
+  
+  def start
+    puts "Welcome Players! A new game is starting."
+    turn_order
   end
 
   def show_score
-
+    puts "P1: #{@player1.lives}/3 vs P@: #{@player2.lives}/3"
   end
 
   def check_score
-
+    if @player1.no_lives_left?
+      declare_winner(@player2)
+    elsif @player2.no_lives_left?
+      declare_winner(@player1)
+    end
   end
 
-  def declare_winner
-
+  def declare_winner(player)
+    puts "#{player.name} wins with a score of #{player.lives}"
+    puts "----- GAME OVER -----"
+    puts "Good bye!"
+    exit(0)
   end
 
   def turn_order
-
-  end
-
-  def start
-
+    @player1.new_question
+    check_score
+    @player2.new_question
+    check_score
+    show_score
+    puts "----- NEW TURN -----"
+    turn_order
   end
 
 end
